49bd7a9df8fe57359acb2e8340ef47e420a4dea7
Amjad Alsharafi <amjadsharafi10@gmail.com>
2022-05-11 13:58:01 +0300

    Added `window` documentation

--- src/window.rs
+++ src/window.rs
@@ -1,3 +1,10 @@
+//! `Window` to display web content in an [`App`].
+//!
+//! An [`App`] can have multiple [`Window`]s, and each [`Window`] can
+//! have multiple [`Overlay`]s managing multiple [`View`]s.
+//!
+//! [`App`]: crate::app::App
+
 use std::{self, ffi::CString};
 
 use crate::{overlay::Overlay, view::View};
@@ -105,15 +112,22 @@ impl TryFrom<ul_sys::ULCursor> for Cursor {
     }
 }
 
+/// Window creation flags
 pub struct WindowFlags {
+    /// Whether the window has borders or not
     pub borderless: bool,
+    /// Whether the window has title or not
     pub titled: bool,
+    /// Whether the window is resizable or not
     pub resizable: bool,
+    /// Whether the window is maximizable or not
     pub maximizable: bool,
+    /// The initial hide/show state of the window
     pub hidden: bool,
 }
 
 impl WindowFlags {
+    /// Helper to convert this struct to a union enum for the C API
     fn to_u32(&self) -> u32 {
         let mut n = 0;
 
@@ -137,12 +151,15 @@ impl WindowFlags {
     }
 }
 
+/// Window struct, represents a platform window.
 pub struct Window {
     internal: ul_sys::ULWindow,
     need_to_destroy: bool,
 }
 
 impl Window {
+    /// Internal function helper to create a view.
+    /// (See [`App::create_window`](crate::app::App::create_window))
     pub(crate) unsafe fn create(
         monitor_raw: ul_sys::ULMonitor,
         width: u32,
@@ -164,6 +181,8 @@ impl Window {
         }
     }
 
+    /// Helper internal function to allow getting a reference to a managed
+    /// session.
     pub(crate) unsafe fn from_raw(raw: ul_sys::ULWindow) -> Self {
         Self {
             internal: raw,
@@ -171,80 +190,105 @@ impl Window {
         }
     }
 
+    /// Get the window width (in screen coordinates).
     pub fn screen_width(&self) -> u32 {
         unsafe { ul_sys::ulWindowGetScreenWidth(self.internal) }
     }
 
+    /// Get the window width (in pixels).
     pub fn width(&self) -> u32 {
         unsafe { ul_sys::ulWindowGetWidth(self.internal) }
     }
 
+    /// Get the window height (in screen coordinates).
     pub fn screen_height(&self) -> u32 {
         unsafe { ul_sys::ulWindowGetScreenHeight(self.internal) }
     }
 
+    /// Get the window height (in pixels).
     pub fn height(&self) -> u32 {
         unsafe { ul_sys::ulWindowGetHeight(self.internal) }
     }
 
+    /// Move the window to a new position (in screen coordinates) relative
+    /// to the top-left of the monitor area.
     pub fn move_to(&self, x: i32, y: i32) {
         unsafe { ul_sys::ulWindowMoveTo(self.internal, x, y) }
     }
 
+    /// Move the window to the center of the monitor.
     pub fn move_to_center(&self) {
         unsafe { ul_sys::ulWindowMoveToCenter(self.internal) }
     }
 
+    /// Get the x-position of the window (in screen coordinates) relative
+    /// to the top-left of the monitor area.
     pub fn x(&self) -> i32 {
         unsafe { ul_sys::ulWindowGetPositionX(self.internal) }
     }
 
+    /// Get the y-position of the window (in screen coordinates) relative
+    /// to the top-left of the monitor area.
     pub fn y(&self) -> i32 {
         unsafe { ul_sys::ulWindowGetPositionY(self.internal) }
     }
 
+    /// Whether or not the window is fullscreen.
     pub fn is_fullscreen(&self) -> bool {
         unsafe { ul_sys::ulWindowIsFullscreen(self.internal) }
     }
 
+    /// The DPI scale of the window.
     pub fn scale(&self) -> f64 {
         unsafe { ul_sys::ulWindowGetScale(self.internal) }
     }
 
+    /// Set the window title.
     pub fn set_title(&self, title: &str) {
         let c_string = CString::new(title).unwrap();
         unsafe { ul_sys::ulWindowSetTitle(self.internal, c_string.as_ptr()) }
     }
 
+    /// Set the cursor.
     pub fn set_cursor(&self, cursor: Cursor) {
         unsafe { ul_sys::ulWindowSetCursor(self.internal, cursor as u32) }
     }
 
+    /// Show the window (if it was previously hidden).
     pub fn show(&self) {
         unsafe { ul_sys::ulWindowShow(self.internal) }
     }
 
+    /// Hide the window.
     pub fn hide(&self) {
         unsafe { ul_sys::ulWindowHide(self.internal) }
     }
 
+    /// Whether or not the window is currently visible (not hidden).
     pub fn is_visible(&self) -> bool {
         unsafe { ul_sys::ulWindowIsVisible(self.internal) }
     }
 
+    /// Close the window.
     pub fn close(&self) {
         unsafe { ul_sys::ulWindowClose(self.internal) }
     }
 
+    /// Convert screen coordinates to pixels using the current DPI scale.
     pub fn screen_to_pixels(&self, val: i32) -> i32 {
         unsafe { ul_sys::ulWindowScreenToPixels(self.internal, val) }
     }
 
+    /// Convert pixels to screen coordinates using the current DPI scale.
     pub fn pixels_to_screen(&self, val: i32) -> i32 {
         unsafe { ul_sys::ulWindowPixelsToScreen(self.internal, val) }
     }
 
     set_callback! {
+        /// Called when the Window is closed.
+        ///
+        /// # Callback Arguments
+        /// * `window: &Window` - The window that fired the event (eg. self)
         pub fn set_close_callback(&self, callback: FnMut(window: &Window)) :
            ulWindowSetCloseCallback(ul_window: ul_sys::ULWindow) {
                let window = &Window::from_raw(ul_window);
@@ -252,6 +296,12 @@ impl Window {
     }
 
     set_callback! {
+        /// Called when the Window is resized.
+        ///
+        /// # Callback Arguments
+        /// * `window: &Window` - The window that fired the event (eg. self)
+        /// * `width: u32` - The new width of the window (in pixels)
+        /// * `height: u32` - The new height of the window (in pixels)
         pub fn set_resize_callback(&self, callback: FnMut(window: &Window, width: u32, height: u32)) :
             ulWindowSetResizeCallback(ul_window: ul_sys::ULWindow, width: u32, height: u32) {
                let window = &Window::from_raw(ul_window);
